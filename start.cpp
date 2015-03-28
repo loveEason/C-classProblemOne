943ba05fdd1b23dbf7b696b22aff6fa6493e4d28
loveEason <756414277@qq.com>
2015-03-28 23:32:44 +0800

    the main function

---
+++ start.cpp
@@ -0,0 +1,30 @@
+#include "header.h"
+using namespace std;
+int main()
+{
+	//P2_16();		//prompt for a digit,then display the digit
+	//P2_26();		//ask user "is it raining now?",then output "it is raining now." if the user input "Y" ,or output "it is not raining now." if the user input "N",or output "is it raining now?"
+	//P2_27();		//ask user "what's your score of the exam(0~100)?",then rank the score
+	//P2_28();		//an easy Menu program
+	//P2_29();		//find out prime numbers between 1 and 100
+	//P2_31();		//precisely display time that user input 
+	//P2_32;		//the same as homework_6();
+	//P2_34();		//there are many red/yellow/blue/white/black balls in pocket.how many the counts if every time take out 3 different colors balls from the pocket
+	//P2_35();		//the same as homework_11();
+	//classP_1();		//the three numbers from big to small order
+	//classP_2();		//display 10 lines of the inverted Pyramid
+	//classP_3();		//bubble sort algorithm,figures from big to small order
+	//homework_1();		//calculate 1+2+3...+n
+	//homework_2();		//calculate 1 - 1/2 + 1/3 - 1/4 + 1/5 - 1/6 ¡­¡­ 1/n
+	//homework_3();		//calculate 1! + 2! + 3! + 4! +¡­¡­+ n!
+	//homework_4();		//calculate 1! + 3! - 5! + 7! - 9! ¡­¡­(2*n+1)!
+	//homework_5();		//calculate sin(x)=x/1! - pow(x,3)/3! + pow(x,5)/5! -pow(x,7)/7! +¡­,finally,the accuracy not less than 0.000001 
+	//homework_6();		//take n numbers from m numbers,calculate counts of combination
+	//homework_7();		//calculate 2*3/4*5*6 + 3*4/5*6*7 - 4*5/6*7*8 + 5*6/7*8*9 -.....finally,the accuracy not less than 0.000001
+	//homework_8();		//calculate 1/2 + 2/3 + 3/5 + 5/8 + 8/13 + 13/21£¬¡­(200 items) 
+	//homework_9();		//calculate PI : PI / 4 = 1 - 1 / 3+ 1 / 5 - 1 / 7 + .....
+	//homework_10();		//display n lines Pyramid
+	//homework_11();		//display positive and negative nine*nine multiplication table
+	//homework_12();		//definite a constant (between 1 and 200)£¬let user guess the constant£¬compare two digits£¬then show the result to user until the user guess the right constant.
+	//homework_13();		//select sort algorithm,figures from big to small order
+}
